f698993eb8a26d61efc7e362cfb78454a21c33b9
Loyalcod <victorakujuobichibuike@gmail.com>
2023-05-29 16:55:49 +0100

    consume student subject api route for manage table, active status and delete api

--- admin/src/components/createStdSubCombo/CreateStdSubCombo.jsx
+++ admin/src/components/createStdSubCombo/CreateStdSubCombo.jsx
@@ -1,85 +1,98 @@
-import { useState, useEffect } from "react"
-import UseAxiosPrivate from "../hooks/UseAxiosPrivate"
-import {ToastContainer, toast} from "react-toastify"
-import {UseAxiosPostPatch} from "../hooks/UseAxiosMethod"
-
-
-
-
-
+import { useState, useEffect } from "react";
+import UseAxiosPrivate from "../hooks/UseAxiosPrivate";
+import { ToastContainer, toast } from "react-toastify";
+import { UseAxiosPostPatch } from "../hooks/UseAxiosMethod";
 
 function CreateStdSubCombo() {
-    const [allstudent, setAllStudent] = useState([])
-    const [allSubject, setAllSubject] = useState([])
-    const [studentId, setStudentId] = useState("")
-    const [subjectId, setSubjectId] = useState("")
+  const [allstudent, setAllStudent] = useState([]);
+  const [allSubject, setAllSubject] = useState([]);
+  const [studentId, setStudentId] = useState("");
+  const [subjectId, setSubjectId] = useState("");
 
-    const secureAxios = UseAxiosPrivate()
-    const AxiosPostPatch = UseAxiosPostPatch()
+  const secureAxios = UseAxiosPrivate();
+  const AxiosPostPatch = UseAxiosPostPatch();
 
-    useEffect(()=>{
-        let isMounted = true
-        const Controller = new AbortController()
-        const getAllStudentSubject = async()=>{
-            try {
-                const response = await secureAxios.get('/student',{signal: Controller.signal})
-                isMounted && setAllStudent(response.data)
-                console.log(response.data)
-                const res = await secureAxios.get('/subject',{signal: Controller.signal})
-                isMounted && setAllSubject(res.data)      
-                console.log(res.data)          
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        getAllStudentSubject()
-        return ()=>{
-            isMounted = false
-            Controller.abort()
-        }
-    },[])
+  useEffect(() => {
+    let isMounted = true;
+    const Controller = new AbortController();
+    const getAllStudentSubject = async () => {
+      try {
+        const response = await secureAxios.get("/student", {
+          signal: Controller.signal,
+        });
+        isMounted && setAllStudent(response.data);
+        console.log(response.data);
+        const res = await secureAxios.get("/subject", {
+          signal: Controller.signal,
+        });
+        isMounted && setAllSubject(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getAllStudentSubject();
+    return () => {
+      isMounted = false;
+      Controller.abort();
+    };
+  }, []);
 
-    const handleClick = async (e)=>{
-        e.preventDefault()
-        if(studentId === "" || subjectId ==="") return toast("pls fill in the space")
-        const reqstData = {studentId, subjectId}
-        AxiosPostPatch('/combo',secureAxios.post,"Student and Subject Assigned Successfully",reqstData)
-
-    }
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (studentId === "" || subjectId === "")
+      return toast("pls fill in the space");
+    const reqstData = { studentId, subjectId };
+    AxiosPostPatch(
+      "/combo",
+      secureAxios.post,
+      "Student and Subject Assigned Successfully",
+      reqstData
+    );
+  };
   return (
     <>
-        <div className="comboContainer">
-            <ToastContainer/>
-            <div className="comboholder">
-                <form onSubmit={handleClick} className="addStdSubComboForm">
-                    <div className="formcontrol">
-                        <label>Select Student:</label><br />
-                        <select value={studentId} onChange={(e)=>setStudentId(e.target.value)}>
-                            <option value="">Select Student</option>
-                            {allstudent.map((student)=>(
-                            <option key={student._id} value={student._id}>{student.fullname} ({student?.studentClassId?.className})</option>
-                            ))}
-                            
-                        </select>
-                    </div>
-                    <div className="formcontrol">
-                        <label>Select Subject:</label><br />
-                        <select value={subjectId} onChange={(e)=>setSubjectId(e.target.value)} >
-                            <option value="">Select Subject</option>
-                            {allSubject.map((subject)=>(
-                            <option key={subject._id} value={subject._id}>{subject.subjectName} ({subject.subjectCode})</option>
-                            ))}
-                            
-                        </select>
-                    </div>
-
-                    <input type="submit" />
-                </form>
+      <div className="comboContainer">
+        <ToastContainer />
+        <div className="comboholder">
+          <form onSubmit={handleClick} className="addStdSubComboForm">
+            <div className="formcontrol">
+              <label>Select Student:</label>
+              <br />
+              <select
+                value={studentId}
+                onChange={(e) => setStudentId(e.target.value)}
+              >
+                <option value="">Select Student</option>
+                {allstudent.map((student) => (
+                  <option key={student._id} value={student._id}>
+                    {student.fullname} ({student?.studentClassId?.className})
+                  </option>
+                ))}
+              </select>
             </div>
+            <div className="formcontrol">
+              <label>Select Subject:</label>
+              <br />
+              <select
+                value={subjectId}
+                onChange={(e) => setSubjectId(e.target.value)}
+              >
+                <option value="">Select Subject</option>
+                {allSubject.map((subject) => (
+                  <option key={subject._id} value={subject._id}>
+                    {subject.subjectName} ({subject.subjectCode})
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <input type="submit" />
+          </form>
         </div>
-        
+      </div>
     </>
-  )
+  );
 }
 
-export default CreateStdSubCombo
+export default CreateStdSubCombo;
--- admin/src/components/manageStdSubComboTable/MangStdSubComboTable.jsx
+++ admin/src/components/manageStdSubComboTable/MangStdSubComboTable.jsx
@@ -1,55 +1,114 @@
 import { DataGrid } from "@mui/x-data-grid";
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { useState, useEffect } from "react";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+import { ToastContainer, toast } from "react-toastify";
+import UseAxiosPrivate from "../hooks/UseAxiosPrivate";
+import { UseAxiosGetAll, UseAxiosDelete } from "../hooks/UseAxiosMethod";
 
 function MangStdSubComboTable() {
-    const rows = [
-        { id: 1, col1: "Hello", col2: "World", col3: "4-10-2022" },
-        {
-          id: 2,
-          col1: "DataGridPro",
-          col2: "is Awesome",
-          col3: "4-10-2022",
-        },
-        {
-          id: 3,
-          col1: "MUI",
-          col2: "is Amazing",
-          col3: "4-10-2022",
-        },
-      ];
+  const columns = [
+    { field: "_id", headerName: "ID", width: 150 },
+    {
+      field: "col2",
+      headerName: "Subject Name/Code ",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <div>
+            {params?.row?.subjectId.subjectName} (
+            {params?.row?.subjectId.subjectCode})
+          </div>
+        );
+      },
+    },
+    {
+      field: "col3",
+      headerName: "Student Name",
+      width: 150,
+      renderCell: (params) => {
+        return <div>{params?.row?.studentId.fullname}</div>;
+      },
+    },
+    {
+      field: "col4",
+      headerName: "Status",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            {params?.row?.status ? (
+              <CheckCircleOutlineIcon
+                onClick={() => toggleStatus(params?.row?._id)}
+                style={{ color: "green", cursor: "pointer" }}
+              />
+            ) : (
+              <CancelOutlinedIcon
+                onClick={() => toggleStatus(params?.row?._id)}
+                style={{ color: "red", cursor: "pointer" }}
+              />
+            )}
+          </>
+        );
+      },
+    },
+    {
+      field: "col5",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <DeleteForeverIcon onClick={()=>handleDelete(params?.row?._id, params?.row?.studentId?._id, params?.row?.subjectId._id)} style={{ color: "red", cursor: "pointer" }} />
+          </>
+        );
+      },
+    },
+  ];
+
+  const secureAxiosApi = UseAxiosPrivate();
+  const AxiosGetApi = UseAxiosGetAll();
+  const [Data, setData] = useState([]);
+  const [activeStatus, setActiveStatus] = useState(false);
+  const [Del, setDel] = useState(false)
+  const delAxiosApi = UseAxiosDelete()
+
+  useEffect(() => {
+    let isMounted = true;
+    let Controller = new AbortController();
+    AxiosGetApi("/combo", secureAxiosApi.get, Controller, isMounted, setData);
+    console.log(Data);
+    return () => {
+      isMounted = false;
+      Controller.abort();
+    };
+  }, [activeStatus, Del]);
+
+  const toggleStatus = async (id) => {
+    setActiveStatus(true);
+    try {
+      const response = await secureAxiosApi.get(`/combo/toggole/${id}`);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+    setActiveStatus(false);
+  };
+
+  const handleDelete = async(id,stdId,subId)=>{
+    delAxiosApi(`/combo/${id}/${stdId}/${subId}`,secureAxiosApi.delete,setDel,"Student Subject Removed successfully")
     
-      const columns = [
-        { field: "col1", headerName: "Student Name", width: 150 },
-        { field: "col2", headerName: "Subject ", width: 150 },
-        { field: "col3", headerName: "Creation Date", width: 150 },
-        {field: "col4", headerName: "Status", width: 150, renderCell: ()=>{
-            return (
-                <>
-                    <CheckCircleOutlineIcon style={{color: "green", cursor:"pointer"}} />
-                </>
-            )
-        }},
-        {
-          field: "col5",
-          headerName: "Action",
-          width: 150,
-          renderCell: () => {
-            return (
-              <>
-                <DeleteForeverIcon style={{color: "red", cursor: "pointer"}}/>
-                
-              </>
-            );
-          },
-        },
-      ];
+  }
+
   return (
     <div className="classTable">
+      <ToastContainer/>
       <div className="tableWrapper">
         <DataGrid
-          rows={rows}
+          rows={Data}
           columns={columns}
+          getRowId={(Data) => Data._id}
           disableRowSelectionOnClick
           checkboxSelection
           pageSize={5}
@@ -57,7 +116,7 @@ function MangStdSubComboTable() {
         />
       </div>
     </div>
-  )
+  );
 }
 
-export default MangStdSubComboTable
+export default MangStdSubComboTable;
